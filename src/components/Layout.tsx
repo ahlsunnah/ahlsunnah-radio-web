5af11d59134c54093f2ff153647c4a20596646d3
abumalick <abdullah.hilson@gmail.com>
2019-06-04 02:07:09 +0000

    feat: remove useless elements in the layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,10 @@
 import 'normalize.css'
 import 'styles/global.scss'
 import React from 'react'
-import {graphql, useStaticQuery} from 'gatsby'
+// import {graphql, useStaticQuery} from 'gatsby'
 import theme from 'styles/theme'
 import {ThemeProvider} from 'emotion-theming'
-import Header from './header'
+// import Header from './header'
 
 interface IProps {
   children: React.ReactNode
@@ -21,27 +21,27 @@ interface ILayoutQuery {
 }
 
 const Layout = ({children}: IProps): JSX.Element => {
-  const {site}: ILayoutQuery = useStaticQuery(graphql`
-    query LayoutQuery {
-      site {
-        siteMetadata {
-          canonicalUrl
-          companyName
-          siteTitle
-        }
-      }
-    }
-  `)
-  const {canonicalUrl, companyName, siteTitle} = site.siteMetadata
+  // const {site}: ILayoutQuery = useStaticQuery(graphql`
+  //   query LayoutQuery {
+  //     site {
+  //       siteMetadata {
+  //         canonicalUrl
+  //         companyName
+  //         siteTitle
+  //       }
+  //     }
+  //   }
+  // `)
+  // const {canonicalUrl, companyName, siteTitle} = site.siteMetadata
   return (
     <ThemeProvider theme={theme}>
       <>
-        <Header siteTitle={siteTitle} />
+        {/* <Header siteTitle={siteTitle} /> */}
         <main>{children}</main>
-        <footer>
+        {/* <footer>
           © {new Date().getFullYear()} Copyright{' '}
           <a href={canonicalUrl}>{companyName}</a> - All rights reserved
-        </footer>
+        </footer> */}
       </>
     </ThemeProvider>
   )
